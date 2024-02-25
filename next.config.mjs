df867f53116792f805afffdb8b4ca0a3ff0bd185
Gavan Wilhite <GavanWilhite@users.noreply.github.com>
2024-02-24 16:16:02 -0800

    hid moderator page and added slideshow redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  async redirects() {
+    return [
+      {
+        source: '/originals',
+        destination: 'https://docs.google.com/presentation/d/1JUhIfgfWp6_M92OQmNMR01qYqPQUhxywCLDYy7qK-uk/edit?usp=sharing',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
